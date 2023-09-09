d5e6e1f1223f37b1da5659c9600d5f642b8e090a
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 01:04:37 +0300

    test: add test for user get selector.

---
+++ only_friends/users/tests/test_selectors.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from users import selectors
+from users.tests.factories import UserFactory
+
+
+class TestSelectors(TestCase):
+    """
+    Test case for user selectors.
+    """
+    def test_user_get(self):
+        new_user = UserFactory()
+        user = selectors.user_get(new_user.pk)
+        self.assertEqual(user, new_user)
