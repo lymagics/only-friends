b9cec1450792f3dabb608e9ce80447f791cb2543
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 00:43:39 +0300

    feat(auth): add jwt token property for user.

--- only_friends/users/models.py
+++ only_friends/users/models.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from core.jwt import jwt_encode
+
 
 class User(AbstractUser):
     """
     User entity.
     """
     email = models.EmailField(unique=True)
+
+    @property
+    def jwt_token(self) -> str:
+        payload = {'id': self.pk}
+        return jwt_encode(payload)
