fe4ae5511b386d02c0c574909f481b26af950cad
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 00:20:06 +0300

    feat(users): add users model.

--- only_friends/config/settings.py
+++ only_friends/config/settings.py
@@ -91,6 +91,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
