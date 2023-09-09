e4ef12d21e4a2c045bd39ac5b268db54db7732f7
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 00:17:02 +0300

    add base drf settings.

--- only_friends/config/settings.py
+++ only_friends/config/settings.py
@@ -97,3 +97,9 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
