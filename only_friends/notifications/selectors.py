eeeb823d0816fdc62b8765ff6510d898b3a013fe
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 12:51:04 +0300

    feat(notify): add notify count selector.

--- only_friends/notifications/selectors.py
+++ only_friends/notifications/selectors.py
@@ -8,3 +8,11 @@ def notify_list(user: User, status: str = 'unseen') -> list[Notification]:
     if status == 'unseen':
         return user.notifications.filter(seen=False).all()
     return user.notifications.all()
+
+
+def notify_count(user: User) -> dict:
+    return {
+        'all': user.notifications.count(),
+        'seen': user.notifications.filter(seen=True).count(),
+        'unseen': user.notifications.filter(seen=False).count(),
+    }
