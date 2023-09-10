b41077f99bb76c046254de137d41b0500d2c0a10
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 16:05:37 +0300

    feat(notify): add endpoint to get notifications count.

--- only_friends/notifications/api/views.py
+++ only_friends/notifications/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from auth.authentication import JWTAuthentication
 from core.decorators import output
@@ -14,3 +15,11 @@ from notifications.api.schemas import NotificationOut
 def notify_list(request):
     status = request.query_params.get('status', 'all')
     return selectors.notify_list(request.user, status=status)
+
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def notify_count(request):
+    count = selectors.notify_count(request.user)
+    return Response(count, status=200)
--- only_friends/notifications/urls.py
+++ only_friends/notifications/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from notifications.api import views
 
 urlpatterns = [
+    path('count/', views.notify_count, name='count'),
     path('', views.notify_list, name='list'),
 ]
 
