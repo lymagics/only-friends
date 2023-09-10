95f3addb6cf830cc72f1abbd27d85d071d46eda7
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-09-10 12:24:34 +0300

    feat(notify): add notification model.

--- only_friends/notifications/models.py
+++ only_friends/notifications/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Notification(BaseModel):
+    """
+    Notification entity.
+    """
+    name = models.CharField(max_length=120)
+    payload = models.JSONField()
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notifications',
+    )
+
+    def __str__(self) -> str:
+        return self.name
